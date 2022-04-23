54aece3988c795d4de0e29788c7b069a9c593923
Marco <marcovelardejr@gmail.com>
2022-04-23 15:44:41 -0500

    commit tree 5a407e368973c614916967c319130f9eb6609ee9

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+
+    return customerName = customerName.toUpperCase()
+
+}
+
+function setBestCustomer() {
+
+    return bestCustomer = 'not bob'; 
+}
+
+function overwriteBestCustomer() {
+
+    return bestCustomer = 'maybe bob';
+}
+
+const LeastFavoriteCustomer = 'hi'; 
+
+function changeLeastFavoriteCustomer(){
+    return LeastFavoriteCustomer = 'yo'; 
+}
+changeLeastFavoriteCustomer();
+console.log (LeastFavoriteCustomer);
